cf8725a77a3d5a1a8bfa20cb9abbeade7efdfc7e
wa2c <entry@wa2c.com>
2022-06-26 00:42:13 +0900

    Add modules

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ buildscript {
         classpath(Deps.Ui.navigationPlugin)
         classpath(Deps.Data.kotlinxSerialization)
         classpath(Deps.Util.ossLicensePlugin)
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
