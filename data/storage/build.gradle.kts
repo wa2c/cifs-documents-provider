a4b4c44298dec098b785832e03824bbfc2bb68d2
wa2c <entry@wa2c.com>
2023-08-20 16:04:20 +0900

    add: storage library

---
+++ data/storage/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id(Deps.App.libraryPlugin)
+    id(Deps.App.kotlinAndroidPlugin)
+}
+
+android {
+    compileSdk = Deps.compileSdkVersion
+    namespace = "${Deps.namespaceBase}.data.storage"
+
+    defaultConfig {
+        minSdk = Deps.minSdkVersion
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = Deps.javaVersionEnum
+        targetCompatibility = Deps.javaVersionEnum
+    }
+
+    kotlin {
+        jvmToolchain {
+            languageVersion.set(JavaLanguageVersion.of(Deps.javaVersion))
+        }
+    }
+}
+
+dependencies {
+    implementation(project(":common"))
+
+    implementation(Deps.App.core)
+    implementation(Deps.App.appcompat)
+
+    testImplementation(Deps.Test.junit)
+    androidTestImplementation(Deps.Test.junitExt)
+}
