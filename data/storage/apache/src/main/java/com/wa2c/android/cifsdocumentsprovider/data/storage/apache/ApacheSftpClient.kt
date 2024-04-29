f258c8b07bc69e5a300ac47d0f549773db9f0c48
wa2c <entry@wa2c.com>
2024-04-29 20:43:59 +0900

    fix

--- data/storage/apache/src/main/java/com/wa2c/android/cifsdocumentsprovider/data/storage/apache/ApacheSftpClient.kt
+++ data/storage/apache/src/main/java/com/wa2c/android/cifsdocumentsprovider/data/storage/apache/ApacheSftpClient.kt
@@ -25,7 +25,7 @@ class ApacheSftpClient(
             builder.setPreferredAuthentications(options, "publickey,password")
             builder.setStrictHostKeyChecking(options, "ask")
             builder.setFileNameEncoding(options, sftpConnection.encoding)
-            builder.setUserDirIsRoot(options, true)
+            builder.setUserDirIsRoot(options, false) // true occurs path mismatch
             builder.setKnownHosts(options, File(knownHostPath))
             // Key
             (sftpConnection.keyData?.encodeToByteArray() ?: sftpConnection.keyFileUri?.let { uri ->
